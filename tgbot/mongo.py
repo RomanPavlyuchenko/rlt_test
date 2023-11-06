8bf1bb9edb97a27163bcb9e052209763dd216132
Roman Pavliuchenko <romanpavlyuchenko@inbox.ru>
2023-11-06 20:53:11 +0300

    get_pipeline update

--- tgbot/mongo.py
+++ tgbot/mongo.py
@@ -1,6 +1,16 @@
 from datetime import datetime
 
 
+FORMATS = {
+    'year': '%Y-01-01T00:00:00',
+    'month': '%Y-%m-01T00:00:00',
+    'day': '%Y-%m-%dT00:00:00',
+    'hour': '%Y-%m-%dT%H:00:00',
+    'minute': '%Y-%m-%dT%H:%M:00',
+    'second': '%Y-%m-%dT%H:%M:%'
+}
+
+
 def get_pipeline(
         date_from: str = '2022-09-01T00:00:00',
         date_to: str = '2022-12-31T23:59:59',
@@ -23,12 +33,31 @@ def get_pipeline(
                 '_id': {
                     'date': {
                         '$dateToString': {
-                            'format': "%Y-%m-01T00:00:00", 'date': "$dt"
+                            'format': FORMATS[group_type],
+                            'date': '$dt'
                         }
                     }
                 },
-                'sum': {'$sum': "$value"}
+                'total': {'$sum': "$value"}
             }
-        }
+        },
+        {
+           '$sort': {'_id': 1}
+        },
+        {
+            '$group': {
+                '_id': None,
+                'dataset': {'$push': '$total'},
+                'labels': {'$push': '$_id.date'}
+            }
+        },
+        {
+            '$project': {
+                '_id': 0,
+                'dataset': 1,
+                'labels': 1
+            }
+        },
+
     ]
     return pipeline
